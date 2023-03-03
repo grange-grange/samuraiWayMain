4ea931985d8a9627e0600f8343647461d8ae1ff2
Alexsander Fisiuk <alexanderfisiuk@gmail.com>
2023-03-03 15:28:17 +0300

    11lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from './Technologies'
-import Footer from "./Footer";
 
 
 function App() {
     return (
-        <div>
-            <Header />
-            <Technologies />
-            <Footer />
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img src={require("./logo.png")}/>
+            </header>
+            <nav className={'nav'}>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className={'content'}>
+                <div>
+                    <img src={require("./profile_bg.jpg")}/>
+                </div>
+                <div>
+                    ava+descrption
+                </div>
+                <div>
+                    my posts
+                </div>
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
         </div>
     )
 }
